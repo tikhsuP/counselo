6d44925d34269676335878da76edf66b84322108
tikhsuP <pushkit20@gmail.com>
2020-04-24 21:37:24 +0530

    imprvmnt

---
+++ src/components/server.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class server extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default server
